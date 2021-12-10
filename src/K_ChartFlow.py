9e8ce494995a53d119dd7f64908bb9f5bbb64f67
hmlu36 <hmlu36@gmail.com>
2021-12-10 17:16:09 +0800

    'deploy'

---
+++ src/K_ChartFlow.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import Utils
+
+'''
+抓取本益比
+取得現今EPS、本益比、近五年六個級距本益比
+
+選股條件：
+1. 本益比小於10
+2. 小於近五年最小級距本益比
+'''
+def GetPE(stockId):
+
+    url = f"https://goodinfo.tw/StockInfo/ShowK_ChartFlow.asp?RPT_CAT=PER&STOCK_ID={stockId}&CHT_CAT=WEEK"
+    df = Utils.GetDataFrameByClass(url, 'b1 p4_0 r0_10 row_bg_2n row_mouse_over');
+    print(df)
+    data = {}
+    '''
+    url = f'https://goodinfo.tw/StockInfo/ShowK_ChartFlow.asp?RPT_CAT=PER&STOCK_ID={stockId}&CHT_CAT=WEEK'
+    resInfo = requests.get(url, headers = GetHeader())
+    resInfo.encoding = 'utf-8'
+    htmlInfo = etree.HTML(resInfo.text)
+
+    header = ['EPS', 'CurrentPE']
+    for index in range(1, 7, 1):
+        #2115
+                #/html/body/table[2]/tbody/tr/td[3]/div/div/div/table/tbody/tr[3]/td[5]
+        XPath = f'/html/body/table[2]/tbody/tr/td[3]/div/div/div/table/tbody/tr[142]/td[{index}]/nobr'
+        target = GetDataByXPath(htmlInfo, XPath)
+        header.append(target)
+    #print(header)
+
+    entry = []
+    for index in range(5, 13, 1):
+        #print(index)
+        XPath = f'/html/body/table[2]/tbody/tr/td[3]/div/div/div/table/tbody/tr[3]/td[{index}]'
+        #print(XPath)
+
+        target = GetDataByXPath(htmlInfo, XPath)
+        entry.append(target)
+
+    #print(entry)
+    data = {}
+    for index in range(len(header)):
+        #print(header[index] + ': ' + data[index])
+        data.update({header[index] : entry[index]})
+
+    '''
+    return data
+
+
+# 測試
+data = GetPE("8112")
+print(data)
+print(list(data)[2] + ' / ' + list(data.values())[2])
--- src/Utils.py
+++ src/Utils.py
@@ -1,14 +1,59 @@
 import re
 from datetime import datetime
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+from BbrowserUserAgent import GetHeader
+
 
 def GetDataByXPath(htmlInfo, XPath):
     return htmlInfo.xpath(re.sub(r'/tbody([[]\\d[]])?', '', XPath) + '/text()')[0]
-    
+
 
 def GetYearBetween(startDateStr, endDate=datetime.today()):
     date_format = "%Y%m%d"
     startDate = datetime.strptime(str(startDateStr), date_format)
     delta = endDate - startDate
     years = round(delta.days / 365)
-    #print(years)
+    # print(years)
     return years
+
+# 取得dataframe比對相等(含有字元), 第一欄值
+
+
+def GetDataFrameValueByLabel(df, columnLable, matchRowLable):
+    return df.set_index(columnLable).filter(like=matchRowLable, axis=0).values[0]
+
+
+def GetDataFrameByClass(url, css):
+    rawData = requests.get(url, headers=GetHeader())
+    rawData.encoding = 'utf-8'
+    soup = BeautifulSoup(rawData.text, "html.parser")
+
+    # 參考
+    # https://stackoverflow.com/questions/50633050/scrape-tables-into-dataframe-with-beautifulsoup
+    table = soup.find('table', attrs={'class': css})
+    table_rows = table.find_all('tr')
+
+    headers = []
+    rows = []
+    for index, tr in enumerate(table_rows):
+        td = tr.find_all('th') + tr.find_all('td')
+        row = [tr.text.strip().replace('\n', '').replace('\xa0', '')
+               for tr in td if tr.text.strip()]
+        if row:
+            if index == 0:
+                headers.append(row)
+            else:
+                rows.append(row)
+
+    # 本益比河流圖有跨欄, 需額外處理(將跨欄標題合併同一行)
+    if 'ShowK_ChartFlow' in url:
+        del headers[0][-1] # 刪除跨欄標題
+        headers[0].extend(rows[0]) #加入第二行跨欄標題
+        del rows[0] # 刪除第二行
+
+    #print(headers)
+    #print(rows)
+    df = pd.DataFrame(rows, columns=headers)
+    return df
