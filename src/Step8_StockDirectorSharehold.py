ccc2aaf352e69b4b9acd06b5b1943ea244ec3820
joshua.lu <hmlu36@gmail.com>
2022-01-05 06:51:21 +0800

    'deploy'

--- src/Step8_StockDirectorSharehold.py
+++ src/Step8_StockDirectorSharehold.py
@@ -6,6 +6,8 @@ import random
 import time
 from datetime import datetime
 
+# https://github.com/rifleak74/MarketDataScience/blob/master/%E7%A8%8B%E5%BC%8F%E9%87%91%E8%9E%8D%E9%A1%9E/%E5%86%A0%E8%BB%8D%E9%81%B8%E8%82%A1%E7%AD%96%E7%95%A5/choose_stock1.py
+
 def GetDirectorSharehold(stockId):
     url = f'https://goodinfo.tw/tw/StockDirectorSharehold.asp?STOCK_ID={stockId}'
     cssSelector = '#divDetail'
