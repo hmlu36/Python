08e4f5b733264d7a0d6716a7cbfe588c9465682d
hmlu36 <hmlu36@gmail.com>
2022-01-05 10:53:10 +0800

    'deploy'

--- src/Step5_ShareholderDistribution2.py
+++ src/Step5_ShareholderDistribution2.py
@@ -5,17 +5,18 @@ import requests
 from datetime import datetime, timedelta
 import re
 from BrowserUserAgent import GetHeader
+from fake_useragent import UserAgent
 
 def GetDistribution(stockId):
     url = 'https://www.tdcc.com.tw/smWeb/QryStockAjax.do'
-    #print(rawData.text)
-    #取得日期下拉選單
+    # print(rawData.text)
+    # 取得日期下拉選單
     payload = {
         'REQ_OPR': 'qrySelScaDates',
     }
 
     dates = requests.post(url, data=payload, headers=GetHeader()).json()
-    #print(dates)
+    # print(dates)
 
     '''
     產生日期區間
@@ -50,20 +51,23 @@ def GetDistribution(stockId):
         tb = soup.select('.mt')[1]
         all_data = []
         for tr in tb.select('tr'):
-            rdata = [td.text.replace("\u3000","").replace(",","").strip() for td in tr.select('td')]
+            rdata = [td.text.replace("\u3000", "").replace(
+                ",", "").strip() for td in tr.select('td')]
             all_data.append(rdata)
 
-        ls_head = ['SEQ', 'LV_DESC', 'NUM_OF_PEOPLE', 'STOCK_SHARES', 'PER_CENT_RT']
-        df = pd.DataFrame(all_data[1:len(all_data)-1], columns=ls_head)	#最後一筆合計資料不要
-        #df.to_csv('股東分布資料.csv',encoding='utf_8_sig')
-        #df = pd.DataFrame(all_data[1:], columns=ls_head)	#最後一筆合計資料不要
-        #print(df)
+        ls_head = ['SEQ', 'LV_DESC', 'NUM_OF_PEOPLE',
+                   'STOCK_SHARES', 'PER_CENT_RT']
+        df = pd.DataFrame(all_data[1:len(all_data)-1],
+                          columns=ls_head)  # 最後一筆合計資料不要
+        # df.to_csv('股東分布資料.csv',encoding='utf_8_sig')
+        # df = pd.DataFrame(all_data[1:], columns=ls_head)	#最後一筆合計資料不要
+        # print(df)
 
         #header = df['LV_DESC'].tolist()
-        #print(header)
+        # print(header)
 
         df = df[['NUM_OF_PEOPLE', 'PER_CENT_RT']]
-        #print(df)
+        # print(df)
         '''
         print(df.iloc[0:1, 0:9])
         people100 = df.iloc[0:1, 0:9].astype(int).sum(axis=1)
@@ -78,38 +82,72 @@ def GetDistribution(stockId):
         #df['100-1000張人數'] = df[['1-999', '1000-5000', '5001-10000', '10001-15000', '15001-20000', '20001-30000', '30001-40000', '40001-50000', '50001-100000']].sum(axis=1)
 
         rangeDicts = {'100張以下': 9, '100-1000張': 14, '1000張以上': 15}
-        #print(df['NUM_OF_PEOPLE'].iloc[0:9])
+        # print(df['NUM_OF_PEOPLE'].iloc[0:9])
 
         row = {}
-        for typeKey, typeValue in {'NUM_OF_PEOPLE':'int', 'PER_CENT_RT':'float'}.items():
+        for typeKey, typeValue in {'NUM_OF_PEOPLE': 'int', 'PER_CENT_RT': 'float'}.items():
             previousCnt = 0
-            
+
             for rangeIndex, (rangeKey, rangeValue) in enumerate(rangeDicts.items()):
                 #print('previousCnt:' + str(previousCnt) + ', rangeValue:' + str(rangeValue))
-                sum = df[typeKey].iloc[previousCnt: rangeValue].astype(typeValue).sum(axis=0)
+                sum = df[typeKey].iloc[previousCnt: rangeValue].astype(
+                    typeValue).sum(axis=0)
                 # 有小數時, 四捨五入小數第二位
                 sum = round(sum, 0 if typeValue == 'int' else 2)
 
-                #print(sum)
-                header = rangeKey + ('人數' if typeKey == 'NUM_OF_PEOPLE' else '比例')
+                # print(sum)
+                header = rangeKey + ('人數' if typeKey ==
+                                     'NUM_OF_PEOPLE' else '比例')
                 row.update({header: sum})
                 if rangeIndex == len(rangeDicts) - 1:
                     previousCnt = rangeValue + 1
                 else:
                     previousCnt = rangeValue
 
-        #print(row)
+        # print(row)
         sum_df = sum_df.append(row, ignore_index=True)
 
     #將dataframe資料轉成 / 串起來
     data = {}
     for column in sum_df.columns.values:
         #print(column + ', data:' + '/'.join(sum_df[column].astype(str)))
-        data.update({column : '  /  '.join(sum_df[column].astype(str))})
+        data.update({column: '  /  '.join(sum_df[column].astype(str))})
 
     df = pd.DataFrame([data])
-    #print(df)
+    # print(df)
     return df
+
+
+def GetDirectorSharehold2(stockId):
+    url = 'https://goodinfo.tw/tw/StockList.asp?SHEET=董監持股&MARKET_CAT=熱門排行&INDUSTRY_CAT=全體董監持股比例'
+    rawData = requests.get(url, headers=GetHeader())
+    rawData.encoding = 'utf-8'
+    print(rawData.text)
+
+def GetDirectorSharehold(stockId):
+    url = 'https://goodinfo.tw/tw/StockList.asp?SHEET=%E8%91%A3%E7%9B%A3%E6%8C%81%E8%82%A1&MARKET_CAT=%E7%86%B1%E9%96%80%E6%8E%92%E8%A1%8C&INDUSTRY_CAT=%E5%85%A8%E9%AB%94%E8%91%A3%E7%9B%A3%E6%8C%81%E8%82%A1%E6%AF%94%E4%BE%8B&RANK=5'
+    url = f'https://goodinfo.tw/tw/StockList.asp'
+    payload = {
+        'SEARCH_WORD': '',
+        'SHEET': '董監持股',
+        'SHEET2': '',
+        'MARKET_CAT': '熱門排行',
+        'INDUSTRY_CAT': '全體董監持股比例(%)@@全體董監@@持股比例(%)',
+        'STOCK_CODE': '',
+        'RPT_TIME': '最新資料',
+        'STEP': 'DATA',
+        'RANK':	5
+    }
+
+    response = requests.get(url, headers=GetHeader())
+    cookies = response.cookies
+    print(cookies.items)
+
+    rawData = requests.post(url, data=payload, cookies=cookies, headers={'user-agent': UserAgent().random, 'Referer': 'https://goodinfo.tw/'})
+    print(rawData.text)
+
+
+GetDirectorSharehold2('2330')
 
 '''
 df = GetDistribution('8112')
--- src/test.py
+++ src/test.py
@@ -1,83 +1,34 @@
-
-import pandas as pd
-from bs4 import BeautifulSoup
+#!/usr/bin/env python3
+# coding=UTF-8
+import math
 import requests
-from datetime import datetime, timedelta
-import re
-from BrowserUserAgent import GetHeader
+from bs4 import BeautifulSoup
+import pandas as pd
 
-def GetDistribution(stockId):
-    url = 'https://www.tdcc.com.tw/smWeb/QryStockAjax.do'
-    url_index = 'https://www.tdcc.com.tw/smWeb/QryStock.jsp'
-    rawData = requests.get(url_index, headers=GetHeader())
-    #print(rawData.text)
-    soup = BeautifulSoup(rawData.text, 'html.parser')
-    data = soup.select_one('#scaDates')
-    print(data)
-    '''
-    today = datetime.today()
-    dateFormat = '%Y/%m/%d'
-    startDateStr = (today - timedelta(days=30)).strftime(dateFormat)
-    endDateStr = today.strftime(dateFormat)
-    # 產生日期區間
-    datePeriod = pd.date_range(startDateStr, endDateStr, freq='W-FRI')[::-1]
-    print('startDate:' + startDateStr + ', endDate:' + endDateStr + ', loopDate:' + datePeriod[0])
-    tempDate = datePeriod[2].strftime('%Y%m%d')
+# 下載網頁內容
+header = {'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'}
 
-    payload = {
-        'scaDates': tempDate,
-        'scaDate': tempDate,
-        'SqlMethod': 'StockNo',
-        'StockNo': stockId,
-        'StockName': '',
-        'REQ_OPR': 'SELECT',
-        'clkStockNo': stockId,
-        'clkStockName': ''
-    }
+VAL_S = 8        #股本範圍起
+VAL_E = 15        #股本範圍迄
 
-    rawData = requests.post(url, data=payload, headers=GetHeader())
+url = 'https://goodinfo.tw/StockInfo/StockList.asp?MARKET_CAT=%E8%87%AA%E8%A8%82%E7%AF%A9%E9%81%B8&INDUSTRY_CAT=%E6%88%91%E7%9A%84%E6%A2%9D%E4%BB%B6&FILTER_ITEM0=K%E5%80%BC+%28%E6%9C%88%29&FILTER_VAL_S0=0&FILTER_VAL_E0=20&FILTER_ITEM1=D%E5%80%BC+%28%E6%9C%88%29&FILTER_VAL_S1=0&FILTER_VAL_E1=20&FILTER_ITEM2=%E9%80%A3%E7%BA%8C%E9%85%8D%E7%99%BC%E7%8F%BE%E9%87%91%E8%82%A1%E5%88%A9%E6%AC%A1%E6%95%B8&FILTER_VAL_S2=5&FILTER_VAL_E2=20&FILTER_ITEM3=%E7%B4%AF%E5%AD%A3%E2%80%93EPS%28%E5%85%83%29&FILTER_VAL_S3=1&FILTER_VAL_E3=5&FILTER_ITEM4=---%E8%AB%8B%E9%81%B8%E6%93%87%E9%81%8E%E6%BF%BE%E6%A2%9D%E4%BB%B6---&FILTER_VAL_S4=&FILTER_VAL_E4=&FILTER_ITEM5=---%E8%AB%8B%E9%81%B8%E6%93%87%E9%81%8E%E6%BF%BE%E6%A2%9D%E4%BB%B6---&FILTER_VAL_S5=&FILTER_VAL_E5=&FILTER_ITEM6=---%E8%AB%8B%E9%81%B8%E6%93%87%E9%81%8E%E6%BF%BE%E6%A2%9D%E4%BB%B6---&FILTER_VAL_S6=&FILTER_VAL_E6=&FILTER_ITEM7=---%E8%AB%8B%E9%81%B8%E6%93%87%E9%81%8E%E6%BF%BE%E6%A2%9D%E4%BB%B6---&FILTER_VAL_S7=&FILTER_VAL_E7=&FILTER_ITEM8=---%E8%AB%8B%E9%81%B8%E6%93%87%E9%81%8E%E6%BF%BE%E6%A2%9D%E4%BB%B6---&FILTER_VAL_S8=&FILTER_VAL_E8=&FILTER_RULE0=---%E8%AB%8B%E6%8C%87%E5%AE%9A%E9%81%B8%E8%82%A1%E6%A2%9D%E4%BB%B6---&FILTER_RULE1=---%E8%AB%8B%E6%8C%87%E5%AE%9A%E9%81%B8%E8%82%A1%E6%A2%9D%E4%BB%B6---&FILTER_RULE2=---%E8%AB%8B%E6%8C%87%E5%AE%9A%E9%81%B8%E8%82%A1%E6%A2%9D%E4%BB%B6---&FILTER_RULE3=---%E8%AB%8B%E6%8C%87%E5%AE%9A%E9%81%B8%E8%82%A1%E6%A2%9D%E4%BB%B6---&FILTER_RULE4=---%E8%AB%8B%E6%8C%87%E5%AE%9A%E9%81%B8%E8%82%A1%E6%A2%9D%E4%BB%B6---&FILTER_RULE5=---%E8%AB%8B%E6%8C%87%E5%AE%9A%E9%81%B8%E8%82%A1%E6%A2%9D%E4%BB%B6---&FILTER_RANK0=---%E8%AB%8B%E6%8C%87%E5%AE%9A%E6%8E%92%E5%90%8D%E6%A2%9D%E4%BB%B6---&FILTER_RANK1=---%E8%AB%8B%E6%8C%87%E5%AE%9A%E6%8E%92%E5%90%8D%E6%A2%9D%E4%BB%B6---&FILTER_RANK2=---%E8%AB%8B%E6%8C%87%E5%AE%9A%E6%8E%92%E5%90%8D%E6%A2%9D%E4%BB%B6---&FILTER_SHEET=%E5%B9%B4%E7%8D%B2%E5%88%A9%E8%83%BD%E5%8A%9B&FILTER_SHEET2=%E7%8D%B2%E5%88%A9%E8%83%BD%E5%8A%9B&FILTER_MARKET=%E4%B8%8A%E5%B8%82%2F%E4%B8%8A%E6%AB%83&FILTER_QUERY=%E6%9F%A5++%E8%A9%A2'
+print(url)
 
-    soup = BeautifulSoup(rawData.text, 'html.parser')
-    tb = soup.select('.mt')[1]
-    all_data = []
-    for tr in tb.select('tr'):
-        rdata = [td.text.replace("\u3000","").replace(",","").strip() for td in tr.select('td')]
-        all_data.append(rdata)
+rt = requests.get(url , headers = header)
 
-    ls_head = ['SEQ', 'LV_DESC', 'NUM_OF_PEOPLE', 'STOCK_SHARES', 'PER_CENT_RT']
-    df = pd.DataFrame(all_data[1:len(all_data)-1], columns=ls_head)	#最後一筆合計資料不要
-    df.to_csv('股東分布資料.csv',encoding='utf_8_sig')
-    #df = pd.DataFrame(all_data[1:], columns=ls_head)	#最後一筆合計資料不要
-    #print(df)
-    header = df['LV_DESC'].tolist()
-    df = df[['NUM_OF_PEOPLE', 'PER_CENT_RT']].T
-    print(df.iloc[0:1, 0:9])
-    people100 = df.iloc[0:1, 0:9].astype(int).sum(axis=1)
-    print(people100)
-    people100_10000 = df.iloc[0:1, 9:14].astype(int).sum(axis=1)
-    print(people100_10000)
+# 確認是否下載成功
+# 以 BeautifulSoup 解析 HTML 程式碼
+    rt.encoding = 'utf-8'
+    soup = BeautifulSoup(rt.text, 'html.parser')
+    df = pd.read_html(str(soup))
 
-    print(df.iloc[0:1, 14:15])
-    people10000 = df.iloc[0:1, 14:15].astype(int).sum(axis=1)
-    print(people10000)
-    #df['100-1000張人數'] = df[['1-999', '1000-5000', '5001-10000', '10001-15000', '15001-20000', '20001-30000', '30001-40000', '40001-50000', '50001-100000']].sum(axis=1)
-    '''
-    '''
-    data = []
-    for rowIndex, option in enumerate(['int','float']):
-        lastCnt = 0
-        for cnt in [9, 14, 15]:
-            print('index:' + str(rowIndex) + ', lastCnt:' + str(lastCnt) + ', cnt:' + str(cnt) )
-            df_row = df.iloc[rowIndex: rowIndex+1, lastCnt : cnt]
-            print(df_row)
-            sum = df_row.astype(option).sum(axis=1)
-            data.append(sum)
-            lastCnt = cnt
+    df = df[17] # Jupyter 測試出來是第17是我們要的 table 
+    df = df[1:] # 只要一列之後
+    df = df[df['代號'].apply(lambda x: str(x).isdigit())] # 刪除非數字的列
+    df = df.reset_index(col_level=1, drop=True) # 因為 drop 後需 index 重新reset 
 
-    print(data)
-    '''
-    df=pd.DataFrame()
-    return df
-
-df = GetDistribution('8112')
-print(df)
+    df['成交'] = df['成交'].astype('float') # 把 str 轉成 float 型態才能sort
+    df = df.sort_values(by='成交', ascending=1)
+    df = df.reset_index(col_level=1, drop=True) # 因為 sort 後需 index 重新reset 
+    df.to_html('goodinfo.html')
+    print(df)
