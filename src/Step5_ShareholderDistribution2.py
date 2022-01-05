37b68d097f7f4a9702bcea9d8d4211dc6fa73dbb
hmlu36 <hmlu36@gmail.com>
2022-01-05 12:00:06 +0800

    'deploy'

--- src/Step5_ShareholderDistribution2.py
+++ src/Step5_ShareholderDistribution2.py
@@ -123,15 +123,26 @@ def GetDistribution(stockId):
 
 
 def GetDirectorSharehold(stockId):
-    url = 'https://goodinfo.tw/tw/StockList.asp?SHEET=董監持股&MARKET_CAT=熱門排行&INDUSTRY_CAT=全體董監持股比例'
     cssSelector = '#divStockList'
-    try:
-        df = Utils.GetDataFrameByCssSelector(url, cssSelector)
-        print(df)
-        df.columns = df.columns.get_level_values(1)
-    except:
-        time.sleep(random.randint(20, 30))
+    sum_df = pd.DataFrame()
 
+    for rankIndex in range(1, 6):
+        url = f'https://goodinfo.tw/tw/StockList.asp?SHEET=董監持股&MARKET_CAT=熱門排行&INDUSTRY_CAT=全體董監持股比例&RANK={str(rankIndex)}'
+        print(url)
+
+        try:
+            #time.sleep(random.randint(20, 30))
+            df = Utils.GetDataFrameByCssSelector(url, cssSelector)
+            print(df)
+            sum_df = pd.concat([sum_df, df], axis=0)
+            #df.columns = df.columns.get_level_values(1)
+        except:
+            #time.sleep(random.randint(20, 30))
+            df = Utils.GetDataFrameByCssSelector(url, cssSelector)
+            print(df)
+            #df.columns = df.columns.get_level_values(1)
+
+    sum_df.to_csv('董監持股比例.csv',encoding='utf_8_sig')
 
 GetDirectorSharehold('2330')
 
