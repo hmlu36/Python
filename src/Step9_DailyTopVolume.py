b8b3d3f93477665e577b4b3a662d20fd32e08dc1
hmlu36 <hmlu36@gmail.com>
2022-01-18 17:10:23 +0800

    'deploy'

--- src/Step9_DailyTopVolume.py
+++ src/Step9_DailyTopVolume.py
@@ -9,7 +9,7 @@ import os
 def GetTopVolume():    
     cssSelector = '#divStockList'
 
-    url = f'https://goodinfo.tw/tw/StockList.asp?MARKET_CAT=熱門排行&INDUSTRY_CAT=成交張數 (高→低)@@成交張數@@由高→低'
+    url = f'https://goodinfo.tw/tw/StockList.asp?RPT_TIME=&MARKET_CAT=熱門排行&INDUSTRY_CAT=日成交張數創近期新高日數@@成交張數@@日成交張數創近期新高日數'
     print(url)
 
     try:
@@ -24,9 +24,10 @@ def GetTopVolume():
     df.columns = df.columns.get_level_values(0)
     df = df.drop_duplicates(keep=False, inplace=False)
     gain = pd.to_numeric(df['漲跌  價'], errors='coerce') > 0
-    market = df['市  場'] == '市'
-    df = df[gain & market]
-    df.to_csv(f'{Utils.GetRootPath()}\Data\Daily\成交量排行.csv', encoding='utf_8_sig')
+    #market = df['市  場'] == '市'
+    length = df['代號'].astype(str).map(len) == 4
+    df = df[gain & length]
+    df.to_csv(f'{Utils.GetRootPath()}\Data\Daily\日成交張數創近期新高日數.csv', encoding='utf_8_sig')
     return df['代號'].values
     # 去除重複標頭
     #sum_df[sum_df.ne(sum_df.columns).any(1)].to_csv(f'{Utils.GetRootPath()}\Data\Monthly\董監持股比例.csv',encoding='utf_8_sig')
