a3625986171b7604229d323c115e982668209942
joshua.lu <hmlu36@gmail.com>
2022-05-14 05:31:03 +0800

    'deploy'

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-worker: python src.main.py
+web: uvicorn src.main:app --host=0.0.0.0 --port=${PORT:-5000}
--- src/main.py
+++ src/main.py
@@ -7,5 +7,5 @@ app = FastAPI()
 def root():
     return {"message": "Hello World!"}
 
-if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", reload=True)
+#if __name__ == "__main__":
+#    uvicorn.run("main:app", host="0.0.0.0", reload=True)
