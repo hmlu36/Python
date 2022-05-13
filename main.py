56e8f0afbe9f4b3efb13bd16fe4306ba09095a95
joshua.lu <hmlu36@gmail.com>
2022-05-14 07:28:55 +0800

    'deploy'

--- main.py
+++ main.py
@@ -8,5 +8,5 @@ def root():
     return {"message": "Hello World!"}
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port="80", reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
     
