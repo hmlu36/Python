aa49dfe1f5997c4bd204dffc1b70adb64cfa3c98
joshua.lu <hmlu36@gmail.com>
2022-05-15 08:32:21 +0800

    'deploy'

--- main.py
+++ main.py
@@ -7,6 +7,6 @@ app = FastAPI()
 def root():
     return {"message": "Hello World!"}
 
-#if __name__ == "__main__":
-#    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
     
