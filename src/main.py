13ecdbc5f920d2cf261a50b84e0998667b058a27
hmlu36 <hmlu36@gmail.com>
2022-05-17 08:27:48 +0800

    'deploy'

--- src/main.py
+++ src/main.py
@@ -49,4 +49,4 @@ def GetChampionStock():
 
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="0.0.0.0", port=80, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
