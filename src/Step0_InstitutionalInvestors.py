342dd117e2cc20f208081449a80d61477fe94028
hmlu36 <hmlu36@gmail.com>
2022-08-04 17:26:24 +0800

    'deploy'

--- src/Step0_InstitutionalInvestors.py
+++ src/Step0_InstitutionalInvestors.py
@@ -1,26 +1,80 @@
-from matplotlib.font_manager import json_load
 import pandas as pd
 import requests
 from datetime import datetime
-from decimal import Decimal
+from dateutil.relativedelta import *
+import time
+import random
 
 # 取出每日收盤價
+# 計算60個交易日
 def GetDailyExchange():
-    now = datetime.now()
-    url = f'https://www.twse.com.tw/fund/BFI82U?response=json&dayDate={now.strftime("%Y%m%d")}&type=day'
-    #print(url)
-    response = requests.get(url)
-    jsonData = response.json()
-    #print(jsonData)
-    #print(jsonData["data"])
-    #print(jsonData["fields"])
-    df = pd.DataFrame(jsonData["data"], columns=jsonData["fields"])
-    df["買賣差額"] = (pd.to_numeric(df["買賣差額"].str.strip().str.replace(",", ""))  / 100000000).round(3)
-    df = df[['單位名稱', '買賣差額']]
-    return df
+    dayCount = 3  # 統計總天數
+    count = 0
+    sum_df = pd.DataFrame()
+    while sum_df.shape[1] < dayCount + 1:
+        tempDate = (datetime.today() - pd.tseries.offsets.BDay(count)).strftime("%Y%m%d")
+        print(tempDate)
+        url = f"https://www.twse.com.tw/fund/BFI82U?response=json&dayDate={tempDate}&type=day"
+        # print(url)
+        response = requests.get(url)
+        jsonData = response.json()
+        # print(jsonData)
+        # print(jsonData["data"])
+        # print(jsonData["fields"])
+        if jsonData["stat"] == "OK":
+            df = pd.DataFrame(jsonData["data"], columns=jsonData["fields"])
+            df["買賣差額"] = (pd.to_numeric(df["買賣差額"].str.strip().str.replace(",", "")) / 100000000).round(3)
+            df = df[["單位名稱", "買賣差額"]]
+            df = df.rename(columns={"買賣差額": tempDate})
+            # print(df)
+
+            if sum_df.empty:
+                sum_df = df
+            else:
+                sum_df = pd.merge(sum_df, df, on=["單位名稱"])
+
+        print(sum_df)
+        count += 1
+        Sleep()
+    return sum_df
+
+
+# ------ 共用的 function ------
+def GetDailyExchangeTotal():
+
+    dayCount = 5  # 統計總天數
+    count = 0
+    sum_df = pd.DataFrame()
+    # print(tempDate + relativedelta(months=-6))
+
+    while sum_df.shape[1] < dayCount + 1:
+        tempDate = datetime.today() - relativedelta(months=count)
+        url = f"https://www.twse.com.tw/exchangeReport/FMTQIK?response=json&date={tempDate.strftime('%Y%m%d')}"
+
+        response = requests.get(url)
+        jsonData = response.json()
+        print(jsonData)
+
+        df = pd.DataFrame(jsonData["data"], columns=jsonData["fields"])
+        df = df[["日期", "成交金額"]]
+        df["成交金額"] = (pd.to_numeric(df["成交金額"].str.strip().str.replace(",", "")) / 100000000).round(3)
+        print(df)
+        df = df.T
+        print(df)
+
+        if sum_df.empty:
+            sum_df = df
+        else:
+            sum_df = pd.merge(sum_df, df, on=["日期"])
+
+        count += 1
+        print(sum_df)
+
+def Sleep():
+    time.sleep(random.randint(5, 10))
 
 
 # ------ 測試 ------
-'''
-print(GetDailyExchange())
-'''
+
+# print(GetDailyExchange())
+GetDailyExchangeTotal()
