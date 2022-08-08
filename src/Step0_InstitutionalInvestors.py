de574af63c6008d7f4a363887a6fd2a143a24e35
joshua.lu <hmlu36@gmail.com>
2022-08-08 20:40:27 +0800

    'deploy'

--- src/Step0_InstitutionalInvestors.py
+++ src/Step0_InstitutionalInvestors.py
@@ -12,8 +12,10 @@ def GetDailyExchange():
     count = 0
     sum_df = pd.DataFrame()
     while sum_df.shape[1] < dayCount + 1:
+        tempDate = (datetime.today() - pd.tseries.offsets.BDay(count)).strftime("%Y%m%d")
         tempDate = (datetime.today() - pd.tseries.offsets.BDay(count))
         print(tempDate)
+        url = f"https://www.twse.com.tw/fund/BFI82U?response=json&dayDate={tempDate}&type=day"
         url = f"https://www.twse.com.tw/fund/BFI82U?response=json&dayDate={tempDate.strftime('%Y%m%d')}&type=day"
         # print(url)
         response = requests.get(url)
@@ -25,27 +27,29 @@ def GetDailyExchange():
             df = pd.DataFrame(jsonData["data"], columns=jsonData["fields"])
             df["買賣差額"] = (pd.to_numeric(df["買賣差額"].str.strip().str.replace(",", "")) / 100000000).round(3)
             df = df[["單位名稱", "買賣差額"]]
-            df = df.rename(columns={"買賣差額": str(tempDate.year - 1911) + '/' + tempDate.strftime("%m/%d")})
+            df = df.rename(columns={"買賣差額": tempDate})
+            df = df.rename(columns={"買賣差額": str(tempDate.year - 1911) + '/' + tempDate.strftime('%m/%d')})
             # print(df)
 
             if sum_df.empty:
                 sum_df = df
             else:
                 sum_df = pd.merge(sum_df, df, on=["單位名稱"])
-                
-        sum_df = sum_df.reset_index().rename(columns={'index':'日期'})
-        # 依日期倒序排序
-        sum_df = sum_df.sort_values(by = '單位名稱', axis = 1, ascending = False)
+
         print(sum_df)
         count += 1
-        #Sleep()
+        Sleep()
+
+        
+    print(sum_df)
     return sum_df
 
 
 # ------ 共用的 function ------
 def GetDailyExchangeTotal():
 
-    dayCount = 6  # 統計總天數
+    dayCount = 5  # 統計總天數
+    dayCount = 60  # 統計總天數
     count = 0
     sum_df = pd.DataFrame()
     # print(tempDate + relativedelta(months=-6))
@@ -61,29 +65,32 @@ def GetDailyExchangeTotal():
         df = pd.DataFrame(jsonData["data"], columns=jsonData["fields"])
         df = df[["日期", "成交金額"]]
         df["成交金額"] = (pd.to_numeric(df["成交金額"].str.strip().str.replace(",", "")) / 100000000).round(3)
-        #print(df)
-        # 轉置
+        print(df)
+        
         df = df.set_index('日期').T
         # 重設index
         df = df.reset_index().rename(columns={'index':'日期'})
         print(df)
-        
+
         if sum_df.empty:
             sum_df = df
         else:
-            sum_df = pd.merge(sum_df, df, on=["日期"])
+            #sum_df = pd.merge(sum_df, df, on=["日期"])
+            sum_df = pd.concat([sum_df, df], axis = 1)
 
         count += 1
-        
-        # 依日期倒序排序
-        sum_df = sum_df.sort_values(by = '日期', axis = 1, ascending = False)
         print(sum_df)
 
+    sum_df = sum_df.sort_values(by ='日期', axis=1, ascending=False)
+    
+    print(sum_df)
+    print(sum_df.shape[1])
+    return sum_df
+
 def Sleep():
-    time.sleep(random.randint(5, 10))
+    time.sleep(random.randint(3, 10))
 
 
 # ------ 測試 ------
-
-print(GetDailyExchange())
-#GetDailyExchangeTotal()
+#GetDailyExchange()
+GetDailyExchangeTotal()
